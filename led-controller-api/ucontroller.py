f5d481ea83bb58fa53b4ceeea4f1461ca4d9e906
Paul Mandal <paul.mandal@gmail.com>
2017-03-11 20:06:38 -0500

    Added code to ucontroller.py to set the configuration on the uController, refactored a bit

--- led-controller-api/ucontroller.py
+++ led-controller-api/ucontroller.py
@@ -10,8 +10,20 @@ from threading import Lock
 serialPort = open(USB_SERIAL, 'w')
 lock = Lock()
 
-def updateLed(json):
-    updateCommand = "U:%d,%d,%d,%d,%d" % (json['ledNumber'], json['red'], json['green'], json['blue'], json['brightness'])
+def setConfig(config, startup=False):
+    ledCount = config['topLedCount'] + config['rightLedCount'] + config['bottomLedCount'] + config['leftLedCount']
+    configCommand = "C:%d" % ledCount
+    send(configCommand)
+    if startup:
+        updateLed(config['startupRed'], config['startupGreen'], config['startupBlue'], config['startupBrightness'])
+    return
+
+def updateLed(ledNumber, red, green, blue, brightness):
+    updateCommand = "U:%d,%d,%d,%d,%d" % (ledNumber, red, green, blue, brightness)
+    send(updateCommand)
+    return
+
+def send(command):
     with lock:
-        serialPort.write(updateCommand)
+        serialPort.write(command)
     return
