f8a31e9386f28ca7a6701df346f48e9ec75f4fdb
Paul Mandal <paul.mandal@gmail.com>
2017-04-15 11:26:53 -0400

    Added pyserial to requirements.txt

--- led-controller-uservice/run-container.sh
+++ led-controller-uservice/run-container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-SVC_NAME=led_controller_uservice
+
+SVC_NAME=led-controller-uservice
 docker stop $SVC_NAME
 docker build . -t $SVC_NAME
 docker run -p 0.0.0.0:31337:31337 --device=/dev/ttyACM0 $SVC_NAME
